0daaa49bba64d484935169ee4724a08143ec9c82
danie <danie@DESKTOP-QGB6P1R>
2025-01-16 11:18:55 +0100

    Add project files.

---
+++ FribergsBilar/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using FribergsBilar.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FribergsBilar.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+    }
+}
